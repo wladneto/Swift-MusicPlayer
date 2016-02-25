cac8e1ed639d1330436c0632b28d7fd8df7333e5
wladimir <wladimirteixeira@yahoo.com.br>
2016-02-24 21:54:09 -0300

    Primeiro commit

--- MusicPlayer/ViewController.swift
+++ MusicPlayer/ViewController.swift
@@ -7,12 +7,74 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer = AVAudioPlayer()
 
+  
+    @IBAction func funcaoPlay(sender: AnyObject) {
+        player.play()
+        
+    }
+    
+    @IBOutlet weak var volumeSlider: UISlider!
+    
+    @IBAction func volumeAjuste(sender: AnyObject) {
+        player.volume = volumeSlider.value
+    }
+    
+    @IBOutlet weak var scrubSlider: UISlider!
+    
+    
+    @IBAction func scrubAjuste(sender: AnyObject) {
+        
+        player.currentTime = NSTimeInterval(scrubSlider.value)
+        
+        
+        
+    }
+    
+    @IBAction func funcaoPausa(sender: AnyObject) {
+        player.pause()
+    
+    }
+    
+    
+    @IBAction func funcaoStop(sender: AnyObject) {
+        player.pause()
+        do {
+            try player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("frango", ofType: "mp3")!))
+            
+        } catch {
+            
+            //Erro
+        }
+
+        
+    }
+    
+    func atualizarTempo () {
+        scrubSlider.value = Float(player.currentTime)
+    
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        do {
+            try player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("frango", ofType: "mp3")!))
+        
+        } catch {
+        
+            //Erro
+        }
+        
+        scrubSlider.maximumValue = Float(player.duration)
+        
+        NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: Selector("atualizarTempo"), userInfo: nil, repeats: true)
+        
     }
 
     override func didReceiveMemoryWarning() {
